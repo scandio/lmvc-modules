16f231e6eb98b5ba4efa02301a12ab3a22bd8fc9
Tobias Deekens <tobias.deekens@gmail.com>
2013-07-02 10:44:10 +0200

    Adds bootstrap and index-action for Sass-module.

---
+++ lib/Scandio/lmvc/modules/sass/controllers/Sass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Scandio\lmvc\modules\sass\controllers;
+
+use Scandio\lmvc\LVC;
+use Scandio\lmvc\Controller;
+
+class Sass extends Controller
+{
+    /**
+     * Controller for a cached SASS compiled style sheet
+     * The cached compiled file will be saved in app root
+     */
+    public static function index()
+    {
+
+    }
+}
