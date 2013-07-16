5172046b7f6702854c17d3e82612daff846b4c3d
Tobias Deekens <tobias.deekens@gmail.com>
2013-07-16 15:06:45 +0200

    Adds stubbed methods and bootstrap file.

---
+++ lib/Scandio/lmvc/modules/upload/controllers/Upload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Scandio\lmvc\modules\upload\controllers;
+
+use Scandio\lmvc\LVC;
+use Scandio\lmvc\Controller;
+
+class Upload extends Controller
+{
+    public static function img(/* reading from stream */) {
+
+    }
+}
