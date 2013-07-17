667c5bff6429f39389e862aeeebdd09f7de6d0b0
Tobias Deekens <tobias.deekens@gmail.com>
2013-07-17 10:10:05 +0200

    Implements methods stubs and adds documentation.

--- lib/Scandio/lmvc/modules/session/Session.php
+++ lib/Scandio/lmvc/modules/session/Session.php
@@ -5,6 +5,36 @@
  */
 class Session
 {
+    protected static
+        $started = false;
+
+
+    /**
+     * Starts the session if it has not been started yet.
+     *
+     * @return bool indicating result of session start.
+     */
+    public static function start()
+    {
+        if (static::$started) { return true; }
+
+        session_start();
+
+        return static::$started;
+    }
+
+    /**
+     * Closes / destroys the session.
+     *
+     * @return bool result of closing session.
+     */
+    public static function close()
+    {
+        if (static::$started) { return session_destroy(); }
+
+        else { return false; }
+    }
+
     /**
      * Sets a value $value at $attr via dot-notation.
      *
@@ -13,7 +43,7 @@ class Session
      */
     public static function set($attr, $value)
     {
-
+        static::setByDotNotation($attr, $value);
     }
 
     /**
@@ -23,7 +53,7 @@ class Session
      */
     public static function get($attr)
     {
-
+        return static::resolveByDotNotation($attr);
     }
 
     /**
@@ -33,7 +63,7 @@ class Session
      */
     public static function replace($array)
     {
-
+        array_replace_recursive($_SESSION, $array);
     }
 
     /**
@@ -43,7 +73,48 @@ class Session
      */
     public static function merge($array)
     {
+        array_merge_recursive($_SESSION, $array);
+    }
 
+    /**
+     * Checks if a value is set in the session and returns a bool indicator not the actual value.
+     *
+     * @param $key requested to be possibly set in session.
+     */
+    public static function has($key)
+    {
+        return ( static::resolveByDotNotation($key) !== null );
+    }
+
+    /**
+     * Flushes the whole session data.
+     */
+    public static function flush()
+    {
+        static::setByDotNotation(null, []);
+    }
+
+    /**
+     * Regenrates the session and its id.
+     *
+     * @param bool $flush indicating if the session should also be flushed (true by default)
+     * @param int|string $lifetime of the session in seconds (null by default)
+     */
+    public static function regenerate($flush = true, $lifetime = null)
+    {
+        if ($lifetime !== null) {
+            ini_set('session.cookie_lifetime', $lifetime);
+        }
+
+        $response = session_regenerate_id($destroy);
+
+        session_write_close();
+        $backup = $flush ? [] : $_SESSION;
+
+        session_start();
+        $_SESSION = $backup;
+
+        return $response;
     }
 
     /**
@@ -52,8 +123,57 @@ class Session
      * @param $dot notation returning the value at the last pointer of the dot-notation.
      * @param array $array to be accessed.
      */
-    private static function resolveDotNotation($dot, $array = null)
+    private static function resolveByDotNotation($dot, $scalar = false)
     {
-        $array = $array == null ? $_SESSION : $array;
+        if (!static::$started) { return null; }
+
+        # Explode the $def string to array by "."
+        $exploded = explode(".", $dot);
+        # Initial pointer to array is the private array variable
+        $arrPointer = $_SESSION;
+
+        # Each subsequent dot-value
+        foreach ($exploded as $explode) {
+            # The value if set, so goto new array pointer
+            if ( isset($arrPointer[$explode]) ) {
+                $arrPointer = $arrPointer[$explode];
+                # Not found the value by dot-notation at given loop run so return null
+            } else {
+                return null;
+            }
+
+            # Finished iterating the array and the value array pointer is pointing to is not another array (except you want a non scalar value)...
+            if ( end($exploded) == $explode && ($scalar === false || !is_array($arrPointer)) ) {
+                return $arrPointer;
+            }
+        }
+    }
+
+    /**
+     * Sets a value in the $_SESSION array by dot notation.
+     *
+     * @param $dot notation for value to be set
+     * @param $value value to be set
+     */
+    private static function setByDotNotation($dot, $value)
+    {
+        if ($dot == null) { $_SESSION = $value; }
+
+        $exploded = explode('.', $dot);
+        $root = &$_SESSION;
+
+        # Loop until end of explodes and reset $_SESSION pointer to $root by reference
+        while (count($exploded) > 1) {
+            $key = array_shift($exploded);
+
+            if (!isset($root[$key])) {
+                $root[$key] = [];
+            }
+
+            $root = &$root[$key];
+        }
+
+        $key = reset($exploded);
+        $root[$key] = $value;
     }
 }
