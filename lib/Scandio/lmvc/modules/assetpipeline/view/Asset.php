7ffb84fb2e26012538d13a7f3b4ad989be3f74d6
Tobias Deekens <tobias.deekens@gmail.com>
2013-08-14 16:34:23 +0200

    Changes view-helper to accept path as paramter.

--- lib/Scandio/lmvc/modules/assetpipeline/view/Asset.php
+++ lib/Scandio/lmvc/modules/assetpipeline/view/Asset.php
@@ -14,16 +14,18 @@ class Asset
      * Note: If the asset pipeline is inactive the call-routing can only handle a string!
      *
      * @param array|array $assets containing asset(s) as in ['jquery.js', 'myplugin.js'] (will be concatenated)
+     * @param string $path for asset(s) to be requested (same for all)
      * @param array $options for asset pipeline as e.g. ['min'] for asset minification
+     *
      * @return string the URI to the requested asset(s)
      */
-    public static function assets($assets, $options = array())
+    public static function assets($assets, $path = '', $options = array())
     {
         # Determines pipe by last file's extension, different files in one request is madness
         $pipe = pathinfo($assets[count($assets) - 1], PATHINFO_EXTENSION);
 
         # Return the url with its options
-        return LVC::get()->url('asset-pipeline::' . $pipe, array_merge($options, $assets));
+        return LVC::get()->url('asset-pipeline::' . $pipe, array_merge($options, array($path), $assets));
     }
 
     /**
